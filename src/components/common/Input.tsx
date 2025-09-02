77d2e7a2edbd89421b12d535e32db741afbca28c
m0hkx <ziechannel0@gmail.com>
2025-09-02 17:13:36 +0300

    Add search top bar

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,9 @@
+function Input() {
+    return (
+        <>
+            <input className="ml-auto w-[90%] rounded-full border-none focus:outline-none px-[0.5vw]" style={{ fontFamily: 'Lufga', fontWeight: 400 }} type="text" placeholder="Search any thing you want..." />
+        </>
+    )
+}
+
+export default Input;
--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -1,8 +1,12 @@
+import Input from "../common/Input";
+import { Search } from "lucide-react";
+
 function Topbar() {
     return (
         <>
-            <div className="col-span-3 bg-[#FFFFFF] w-[90%] rounded-full">
-
+            <div className="col-span-3 bg-[#FFFFFF] w-[90%] rounded-full flex justify-center items-center pl-[1vw]">
+                <Search />
+                <Input />
             </div>
 
             <div className="col-start-10 col-span-3 bg-[#FFFFFF] rounded-full w-[90%] ml-auto">
