176e70e4752286f8fb5892193dc5ccbca89e8aa4
m0hkx <ziechannel0@gmail.com>
2025-09-02 13:06:22 +0300

    Some edits on the UI

--- src/components/layout/Overview.tsx
+++ src/components/layout/Overview.tsx
@@ -1,25 +1,28 @@
+import Topbar from "./Topbar";
+
 function Overview() {
     return (
         <>
-        <div className="grid grid-cols-12 grid-rows-13 gap-[24px] px-[5vw] w-full h-full">
+        <div className="grid grid-cols-12 grid-rows-14 gap-[12px] px-[5vw] py-[1vw] w-full h-full">
+            <Topbar />
             <h1 className="row-start-2 col-span-2 self-end-safe text-4xl" style={{ fontFamily: 'Lufga', fontWeight: 600 }}>Real Estate</h1>
-            <div className="bg-[#000000] row-start-3 row-span-5 col-span-6 rounded-[40px]">
+            <div className="bg-[#FFFFFF] row-start-3 row-span-6 col-span-6 rounded-[40px] drop-shadow-[0_0_2px_rgba(0,0,0,0.25)]">
 
             </div>
 
-            <div className="bg-[#000000] row-start-3 row-span-3 col-start-7 col-span-3 rounded-[40px]">
+            <div className="bg-[#FFFFFF] row-start-3 row-span-4 col-start-7 col-span-3 rounded-[40px] drop-shadow-[0_0_2px_rgba(0,0,0,0.25)]">
 
             </div>
 
-            <div className="bg-[#000000] row-start-3 row-span-3 col-start-10 col-span-3 rounded-[40px]">
+            <div className="bg-[#FFFFFF] row-start-3 row-span-4 col-start-10 col-span-3 rounded-[40px] drop-shadow-[0_0_2px_rgba(0,0,0,0.25)]">
 
             </div>
 
-            <div className="bg-[#000000] row-start-6 row-span-7 col-start-7 col-span-6 rounded-[40px]">
+            <div className="bg-[#FFFFFF] row-start-7 row-span-8 col-start-7 col-span-6 rounded-[40px] drop-shadow-[0_0_2px_rgba(0,0,0,0.25)]">
 
             </div>
 
-            <div className="bg-[#000000] row-start-8 row-span-5 col-span-6 rounded-[40px]">
+            <div className="bg-[#FFFFFF] row-start-9 row-span-6 col-span-6 rounded-[40px] drop-shadow-[0_0_2px_rgba(0,0,0,0.25)]">
 
             </div>
         </div>
---
+++ src/components/layout/Topbar.tsx
@@ -0,0 +1,19 @@
+function Topbar() {
+    return (
+        <>
+            <div className="col-span-3 bg-[#FFFFFF] w-[90%] rounded-full">
+
+            </div>
+
+            <div className="col-start-10 col-span-3 bg-[#FFFFFF] rounded-full w-[90%] ml-auto">
+
+            </div>
+
+            <div className="col-start-9 col-span-1 row-start-1 bg-[#FFFFFF] rounded-full">
+
+            </div>
+        </>
+    )
+}
+
+export default Topbar;
